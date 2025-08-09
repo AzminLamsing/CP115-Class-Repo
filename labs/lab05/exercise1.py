181902b4ab0c3256e1a6a387a7bf000447d0ba5f
AzminLamsing <mohammadazminlamzing>
2025-08-10 02:23:04 +0800

    lab5

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,25 @@
+# Examples of valid identifiers and variables
+student_name = "Alice"
+age = 20
+is_enrolled = True
+course_code = "CP115"
+
+# Getting user input (always returns a string)
+age = input("Enter your age: ")
+student_name = input("Enter your name: ")
+course_code = input("Enter the course code: ")
+
+# String data type
+student_name = "Alice"    # str (string)
+course_code = "CP115"  # str (single or double quotes)
+
+## Try this
+print(type(age))
+print(type(student_name))
+print(type(course_code))
+
+
+# Formatted output using string concatenation
+print()
+print("Student: " + student_name)
+print("Course: " + course_code)
