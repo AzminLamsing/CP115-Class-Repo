70bc90e524f4913178f0d991966198182fd5a14e
AzminLamsing <mohammadazminlamzing>
2025-08-11 06:41:29 +0800

    tt

---
+++ labs/lab05/exercise5.py
@@ -0,0 +1,16 @@
+# Average Score Calculator
+
+#Getting inpur from the user
+
+testscore1 = float(input("Enter Your Score1"))
+testscore2 = float(input("Enter Your Score2"))
+testscore3 = float(input("Enter Your Score3"))
+
+totalscore = testscore1 + testscore2 + testscore3
+averagescore = (totalscore / 3)
+
+print("testscore1" , testscore1)
+print("testscore2" , testscore2)
+print("testscore3" , testscore3)
+print("Your Total Score Is" , totalscore)
+print("Your Average Score Is" , averagescore)
