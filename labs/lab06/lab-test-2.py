0a13cdcf351bb4d7c56196b93454bfafa502f06d
AzminLamsing <mohammadazminlamzing>
2025-08-18 13:13:55 +0800

    hello

---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,14 @@
+# Input
+
+print("Name:Ahmad Bin Abu\t                          Matric.No:Ms2025123499")
+print("* \t                *")
+print("**  \t         **")
+print("***   \t ***")
+print("********")
+print("***   \t ***")
+print("**  \t         **")
+print("* \t                *")
+
+
+print("This is my\n\tsecond\n\t\tassignment")
+print("I want 2x10 marks,which is 20 full marks")
