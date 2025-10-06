5956d4fc295a195ce4a3fe721767d26391785eb1
AzminLamsing <mohammadazminlamzing>
2025-10-06 11:32:51 +0800

    hello

--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -2,7 +2,25 @@ age = int(input("Enter your age : "))
 accident_count = int(input("Enter your accident count : "))
 
 # Your code here
-if(age )
+if(age <= 25):
+    base_premium = 2400
+elif(age < 51 and age > 25):
+    base_premium = 1800
+else:
+    base_premium = 2000
+
+accident_count = 0
+
+if(accident_count == 0):
+    discount_amount = 0.9 
+elif(accident_count <= 2):
+    accidents = 300
+else:
+    accidents = 600
+
+final_premium = base_premium + (accidents 
+
+
 
 print(base_premium)
 print(final_premium)
--- labs/lab11/exercise1/exercise1.py
+++ labs/lab11/exercise1/exercise1.py
@@ -1,7 +1,21 @@
-num_rounds = int(input())
+num_rounds = int(input("Enter your number rounds : "))
 
 # TODO: Your code here
 # Use input() inside the loop to get each round's score
+num_rounds = 0
+round_processed = 0
+
+for i in range(num_rounds):
+    print()
+
+
+    if(round_processed <= 100):
+        point = round_processed
+    else:
+        point = round_processed + (round_processed * 0.2)
+
+    
+
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
