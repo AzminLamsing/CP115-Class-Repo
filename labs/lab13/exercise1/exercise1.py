98011498a6f9190624617becc1a812aa50680ab4
AzminLamsing <mohammadazminlamzing>
2025-10-27 12:56:06 +0800

    lab 13

--- labs/lab13/exercise1/exercise1.py
+++ labs/lab13/exercise1/exercise1.py
@@ -1,6 +1,26 @@
 correct_password = "python123"
 
 # TODO: Your code here
+login_successful = False
+attempts_used = 0
+
+for i in range(3):
+    attempts_used = i + 1  # Track the current attempt number
+    
+    # Get password from user
+    user_input = input("Enter password: ")
+    
+    # Check if the password is correct
+    if user_input == correct_password:
+        login_successful = True
+        print("Access granted!")
+        break  # Stop the loop immediately
+    else:
+        # Only print 'denied' if it's not the last attempt
+        if attempts_used < 3:
+            print("Access denied. Try again.")
+        else:
+            print("Access denied. No attempts remaining.")
 
 print(login_successful)
 print(attempts_used)
--- labs/lab13/exercise2/exercise2.py
+++ labs/lab13/exercise2/exercise2.py
@@ -1,3 +1,11 @@
 # TODO: Your code here
+found_number = 0
+
+for i in range (1,101):
+
+    if (num % 7 == 0 and num % 13 == 0) :
+        found_number == num 
+        break
+
 
 print(found_number)
